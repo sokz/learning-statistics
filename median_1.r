ed07b82348bccb22a206ba3821264b7f271fe4b0
Sonith Kumar <sonith.kumar@gmail.com>
2018-12-23 23:04:48 +0530

    Added median_1.r (to demonstrate the median concept)

---
+++ median_1.r
@@ -0,0 +1,14 @@
+# median - number that seperates ordered data into halves
+# - half are same size or smaller
+# -  alf are the same size or larger than the median
+ 
+x = c(179, 160, 136, 227, 217, 168, 108, 124, 143, 140)
+y = sort(x)
+
+(143+160)/2
+
+x = x = c(179, 160, 136, 227, 217, 168, 108, 124, 143, 140, 500)
+y = sort(x)
+
+mean(x)
+median(x)
