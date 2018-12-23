ac8cd451df4b8f84831520b8a751c502bb549de4
Sonith Kumar <sonith.kumar@gmail.com>
2018-12-23 22:46:36 +0530

    Added mean_1.r (shows the function of mean() function)

---
+++ mean_1.r
@@ -0,0 +1,10 @@
+# R script showing the usage of mean() keyword
+# mean() - used to find mean of the provided data
+# c() - adding data to variable
+
+x = c(179, 160, 136, 227, 217, 168, 108, 124, 143, 140)
+sum(x)
+# sum() - sum the data
+sum(x)/10
+
+mean(x)
